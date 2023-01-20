5bfbf03c541b91695cab65445c7a81d5236f4d8e
djibrilnaji <najidjibril@gmail.com>
2023-01-20 22:33:53 +0100

    AP-67 : improve contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar"
 import Head from "next/head"
 import Footer from "../components/Footer"
+import Input from "../components/Input"
 
 const contact = () => {
     return (
@@ -15,27 +16,12 @@ const contact = () => {
                 >
                     <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
                         <form className="border-2 p-10 rounded-2xl">
-                            <div
-                                className="font-semibold opacity-70 text-2xl font-normal text-center pb-5 uppercase">
+                            <h1 className="font-semibold opacity-70 text-2xl font-normal text-center pb-5 uppercase z-0">
                                 Email Us
-                            </div>
+                            </h1>
 
-                            <div className="mb-6">
-                                <input
-                                    type="e-mail"
-                                    className="form-control block w-full px-4 py-2 text-lg font-normal text-gray-700 bg-white border border-solid border-gray-300 rounded-lg transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-stone-500 focus:outline-none"
-                                    placeholder="E-mail*"
-                                />
-                            </div>
-
-                            <div className="mb-6">
-                                <input
-                                    type="text"
-                                    className="form-control block w-full px-4 py-2 text-lg font-normal text-gray-700 bg-white border border-solid border-gray-300 rounded-lg transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-stone-500 focus:outline-none"
-                                    placeholder="Subject"
-                                />
-                            </div>
-
+                            <Input type="email" placeholder="E-mail*"/>
+                            <Input type="text" placeholder="Subject"/>
                             <div className="mb-6">
                                 <textarea
                                     className="form-control block w-full px-4 py-2 text-lg font-normal text-gray-700 bg-white border border-solid border-gray-300 rounded-lg transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-stone-500 focus:outline-none"
@@ -43,12 +29,12 @@ const contact = () => {
                                 />
                             </div>
 
+
                             <div className="text-center">
 
                                 <button
                                     type="button"
                                     className="px-10 py-3 bg-stone-400 text-white text-sm uppercase rounded-full"
-
                                 >
                                     Envoyer
                                 </button>
