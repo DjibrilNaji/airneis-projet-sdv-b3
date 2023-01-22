66c569ba7deb30a3be427e0170cfc5dfcf6efc33
djibrilnaji <najidjibril@gmail.com>
2023-01-22 18:13:15 +0100

    rework !

--- src/components/Options.jsx
+++
@@ -1,7 +0,0 @@
-const Options = (props) => {
-  const { label } = props
-
-  return <option>{label}</option>
-}
-
-export default Options
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      spacing: {
-        small: "5%",
-        middlemedium: "20%",
-        medium: "10%",
-        long: "30%",
-        superlong: "100%",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
+    plugins: [],
 }
