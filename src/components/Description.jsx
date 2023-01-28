27d021465e9115a504280588e7b098f71dd7fc62
djibrilnaji <najidjibril@gmail.com>
2023-01-28 22:21:23 +0100

    AP-80 : Delete useless components

--- src/components/Description.jsx
+++
@@ -1,9 +0,0 @@
-import classNames from "classnames"
-
-const Description = (props) => {
-    const { className, ...otherProps } = props
-  
-    return <p {...otherProps} className={classNames("", className)} />
-  }
-  
-  export default Description
--- src/components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import classNames from "classnames"
-
-const Title = (props) => {
-    const { className, ...otherProps } = props
-  
-    return <h1 {...otherProps} className={classNames("", className)} />
-  }
-  
-  export default Title
