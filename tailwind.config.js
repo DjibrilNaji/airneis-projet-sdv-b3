9eb37ba2217b503ed488da1fac215fe64a0a94d5
Alexis Duchemann <75632261+aeronmen2@users.noreply.github.com>
2023-01-31 10:51:26 +0100

    AP-96 : Importing Quicksand font to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
