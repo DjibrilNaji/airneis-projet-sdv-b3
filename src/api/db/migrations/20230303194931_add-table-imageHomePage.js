d5b690f2ce9b95c05979d27aa91751229436c066
djibrilnaji <najidjibril@gmail.com>
2023-03-05 18:31:33 +0100

    AP-128 : update migration

--- src/api/db/migrations/20230303194931_add-table-imageHomePage.js
+++ src/api/db/migrations/20230303194931_add-table-imageHomePage.js
@@ -2,7 +2,7 @@ module.exports.up = async (knex) => {
   await knex.schema.createTable("imageHomePage", (table) => {
     table.increments("id")
     table.text("urlImage").notNullable()
-    table.boolean("display").notNullable()
+    table.boolean("display").notNullable().defaultTo(0)
   })
 }
 
