231d1c6947aa84b3f6fbbca44c8fc350148860ed
Myriam BENABDESSADOK <myriamb1512@gmail.com>
2023-03-12 19:36:09 +0100

    First try extraction of order number

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-const { resolve } = require("node:path")
+const { resolve } = require("path")
 const { config } = require("dotenv")
 
 config()
--- src/pages/confirmation.jsx
+++ src/pages/confirmation.jsx
@@ -2,9 +2,48 @@
 
 import Button from "@/web/components/Button"
 import Link from "@/web/components/Link"
-// import axios from "axios"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
-const Confirmation = () => {
+export const getServerSideProps = ({ params }) => ({
+  props: {
+    params: {
+      trackingNumber: params.trackingNumber,
+    },
+  },
+})
+
+const Confirmation = (props) => {
+  const [OrderNumber, setOrderNumber] = useState([])
+
+  const {
+    params: { trackingNumber },
+  } = props
+
+  const OrderStatus = async () => {
+    getServerSideProps({ params: `localhost:3000/users/order/` })
+    const result = await axios.get("/api/orders")
+
+    if (result.data.map((order) => order.status) === "paid") {
+      setOrderNumber(
+        result.data.find((order) => order.trackingNumber === trackingNumber)
+          .trackingNumber
+      )
+    }
+
+    console.log(
+      "Command number",
+      result.data.find((order) => order.trackingNumber === trackingNumber)
+        .trackingNumber
+    )
+  }
+
+  useEffect(() => {
+    OrderStatus()
+  })
+  //Pour l'affichage comme ça vient après le paiement faut voir comment l'API de paiement intergait avec la BDD pour changer le statut de la commande
+  //cette fonction est à faire également dans la page BDD
+
   return (
     <>
       <h1 className="flex text-3xl justify-center py-5 font-bold md:justify-center">
@@ -12,12 +51,11 @@ const Confirmation = () => {
       </h1>
       <div>
         <p className="flex justify-center items-center md:w-auto">
-          Votre commande a bien été enregistrée sous le numéro XXXXXXX .
+          Votre commande a bien été enregistrée sous le numéro {OrderNumber}.
         </p>
         <p className="flex justify-center items-center md:w-auto">
           Vous pouvez suivre son état depuis votre espace client.
         </p>
-        {/* Message provisoire en attendant le back pour XXXXXXX*/}
         <div className="flex justify-center py-7 md:justify-center items-center">
           <Link href="/cart">
             <Button>Continuer mes achats</Button>
