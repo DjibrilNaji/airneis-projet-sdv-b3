b1c88b42291a3d77d83d3a0ab25c355242a1942a
djibrilnaji <najidjibril@gmail.com>
2023-03-29 22:13:22 +0200

    Add pagination

--- src/api/db/models/UserModel.js
+++ src/api/db/models/UserModel.js
@@ -6,6 +6,12 @@ import hashPassword from "../hashPassword.js"
 class UserModel extends BaseModel {
   static tableName = "users"
 
+  static modifiers = {
+    paginate: (query, limit, page) => {
+      return query.limit(limit).offset((page - 1) * limit)
+    },
+  }
+
   static relationMappings() {
     return {
       address: {
