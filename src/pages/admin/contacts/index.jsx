f5f45124ad134977650a05b49802107d359627aa
djibrilnaji <najidjibril@gmail.com>
2023-04-08 16:21:26 +0200

    add select to choose how many messages can be visble and add handle sortChange

--- src/pages/admin/contacts/index.jsx
+++ src/pages/admin/contacts/index.jsx
@@ -15,13 +15,14 @@ const ContactAdmin = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState("")
 
+  const [sortColumn, setSortColumn] = useState("id")
   const [order, setOrder] = useState("asc")
   const [limit, setLimit] = useState(10)
 
   const fetchData = useCallback(
     async (page) => {
       const result = await axios.get(
-        `/api/contact?limit=${limit}&page=${page}&order=${order}`
+        `/api/contact?limit=${limit}&page=${page}&sortColumn=${sortColumn}&order=${order}`
       )
 
       const totalMessages = result.data.result.meta.count
@@ -30,7 +31,7 @@ const ContactAdmin = () => {
       setTotalPages(totalPages)
       setData(result.data.result)
     },
-    [order, limit]
+    [order, limit, sortColumn]
   )
 
   useEffect(() => {
@@ -43,6 +44,29 @@ const ContactAdmin = () => {
       fetchData(newPage)
     },
     [fetchData]
+  )
+
+  const handleLimitChange = useCallback(
+    (e) => {
+      setLimit(e.target.value)
+      setCurrentPage(1)
+      fetchData(1)
+    },
+    [fetchData]
+  )
+
+  const handleSortChange = useCallback(
+    (column) => {
+      if (column === sortColumn) {
+        setOrder(order === "asc" ? "desc" : "asc")
+      } else {
+        setSortColumn(column)
+        setOrder("asc")
+      }
+
+      fetchData(currentPage)
+    },
+    [fetchData, currentPage, order, sortColumn]
   )
 
   const pagination = []
@@ -88,13 +112,41 @@ const ContactAdmin = () => {
         </div>
       </div>
 
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2 my-6 mx-1">
+          <span>Show</span>
+          <select
+            name="country"
+            className="px-1 border-2 rounded-lg md:px-3 text-right focus:outline-none"
+            value={limit}
+            onChange={handleLimitChange}
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+            <option value="25">25</option>
+            <option value="30">30</option>
+          </select>
+          <span>users per page</span>
+        </div>
+      </div>
+
       <table className="w-[100vw]">
         <thead className="text-xs text-left uppercase bg-gray-50 text-gray-700">
           <tr>
             <th className="py-2 px-1">Select</th>
-            <TableHeadField displayName="Id" />
-            <TableHeadField displayName="Email" />
-            <TableHeadField displayName="Message" />
+            <TableHeadField
+              displayName="Id"
+              handleSortChange={handleSortChange}
+              fieldName="id"
+            />
+            <TableHeadField
+              displayName="Email"
+              handleSortChange={handleSortChange}
+              fieldName="email"
+            />
+            <th className="py-2 px-4">Message</th>
             <th className="py-2 px-4">More</th>
           </tr>
         </thead>
