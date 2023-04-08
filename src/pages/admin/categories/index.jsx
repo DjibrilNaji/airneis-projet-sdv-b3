c2720448bc4f623fc0b2b083fa426e8a65624d47
Alexis Duchemann <75632261+aeronmen2@users.noreply.github.com>
2023-04-08 12:15:31 +0200

    added categories admin page

--- src/pages/admin/categories/index.jsx
+++ src/pages/admin/categories/index.jsx
@@ -1,64 +1,205 @@
 import LayoutAdmin from "@/web/components/Admin/LayoutAdmin/LayoutAdmin"
 import axios from "axios"
 import Link from "next/link"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faArrowLeft,
+  faArrowRight,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons"
+import TableHeadField from "@/web/components/Admin/TableHeadField"
+import routes from "@/web/routes"
 
 const CategoriesAdmin = () => {
   const [data, setData] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState("")
+
+  const [sortColumn, setSortColumn] = useState("id")
+  const [order, setOrder] = useState("asc")
+  const [limit, setLimit] = useState(10)
+  const [searchTerm, setSearchTerm] = useState(null)
+
+  const fetchData = useCallback(
+    async (page) => {
+      const result = await axios.get(
+        `/api${routes.api.admin.categories()}?limit=${limit}&page=${page}&sortColumn=${sortColumn}&order=${order}` +
+          (searchTerm === null ? "" : `&searchTerm=${searchTerm}`)
+      )
+
+      const totalCategories = result.data.result.meta.count
+      const totalPages = Math.ceil(totalCategories / limit)
+
+      setTotalPages(totalPages)
+      setData(result.data.result)
+    },
+    [order, sortColumn, limit, searchTerm]
+  )
 
   useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get("/api/categories")
-      setData(result.data.result)
-    }
-    fetchData()
-  }, [])
+    fetchData(currentPage)
+  }, [currentPage, fetchData])
+
+  const handlePageChange = useCallback(
+    (newPage) => {
+      setCurrentPage(newPage)
+      fetchData(newPage)
+    },
+    [fetchData]
+  )
+
+  const handleLimitChange = useCallback(
+    (e) => {
+      setLimit(e.target.value)
+      fetchData
+    },
+    [fetchData]
+  )
+
+  const handleSortChange = useCallback(
+    (column) => {
+      if (column === sortColumn) {
+        setOrder(order === "asc" ? "desc" : "asc")
+      } else {
+        setSortColumn(column)
+        setOrder("asc")
+      }
+
+      fetchData(currentPage)
+    },
+    [fetchData, currentPage, order, sortColumn]
+  )
+
+  const pagination = []
+  for (let i = 1; i <= totalPages; i++) {
+    pagination.push(
+      <button
+        key={i}
+        className={`h-12 border-2 border-r-0 border-stone-500
+               w-12  ${currentPage === i && "bg-stone-500 text-white"}`}
+        onClick={() => handlePageChange(i)}
+      >
+        {i}
+      </button>
+    )
+  }
 
   return (
     <>
-      <div className="flex w-full mb-5 items-center">
-        <span className="font-extrabold text-2xl uppercase">Categories</span>
-        <Link
-          href="/admin/categories/create"
-          className="bg-white p-4 ml-auto mr-0 border-2 rounded-lg hover:bg-slate-200 "
-        >
-          Create category
-        </Link>
+      <div className="flex w-full justify-center mb-5">
+        <span className="font-extrabold text-3xl text-stone-500 uppercase">
+          Categories
+        </span>
       </div>
 
-      <div>
-        <table className="w-[80vw] text-sm text-left text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-              <th className="px-6 py-3">Name</th>
-              <th className="px-6 py-3">Description</th>
-              <th className="px-6 py-3">Modify</th>
-              <th className="px-6 py-3">More</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.categories?.map((category) => (
-              <tr key={category.id}>
-                <td className="px-6 py-4">{category.name}</td>
-                <td className="px-6 py-4">{category.description}</td>
-                <td className="px-6 py-4">
-                  <Link href={`/admin/categories/${category.id}`} className="">
-                    Modify
-                  </Link>
-                </td>
-                <td className="px-6 py-4">
-                  <Link
-                    href={`/admin/categories/modify/${category.id}`}
-                    className=""
-                  >
-                    See category...
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="flex justify-center my-5">
+        <div className="flex">
+          <button
+            className={
+              "h-12 border-2 border-r-0 text-stone-500  border-stone-500 px-4 rounded-l-lg hover:bg-stone-500 hover:text-white disabled:opacity-30 disabled:z-[-1]"
+            }
+            disabled={currentPage === 1}
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <div> {pagination}</div>
+          <button
+            className="h-12 border-2 text-stone-500  border-stone-500 px-4 rounded-r-lg hover:bg-stone-500 hover:text-white disabled:opacity-30 disabled:z-[-1]"
+            disabled={currentPage === totalPages}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
       </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2 my-6">
+          <span>Show</span>
+          <select
+            name="country"
+            className="border-2 rounded-lg px-3 text-right"
+            value={limit}
+            onChange={handleLimitChange}
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+            <option value="25">25</option>
+            <option value="30">30</option>
+          </select>
+          <span>Categories per page</span>
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="border-2 border-stone-500 rounded-lg px-2 focus:outline-none"
+            value={searchTerm == null ? "" : searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <table className="w-full">
+        <thead className="text-xs text-left uppercase bg-gray-50 text-gray-700">
+          <tr>
+            <th className="py-2 px-4">Select</th>
+            <TableHeadField
+              displayName="Id"
+              handleSortChange={handleSortChange}
+              fieldName="id"
+            />
+            <TableHeadField
+              displayName="Name"
+              handleSortChange={handleSortChange}
+              fieldName="name"
+            />
+            <TableHeadField
+              displayName="Description"
+              handleSortChange={handleSortChange}
+              fieldName="description"
+            />
+            <TableHeadField
+              displayName="Slug"
+              handleSortChange={handleSortChange}
+              fieldName="slug"
+              className="hidden md:table-cell"
+            />
+            <th className="py-2 px-4">More</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.categories?.map((category) => (
+            <tr key={category.id} className="border-b text-sm border-gray-300">
+              <td className="py-2 px-4">
+                <input
+                  type="checkbox"
+                  className="h-5 w-5 border-2 appearance-none checked:bg-stone-500 cursor-pointer"
+                />
+              </td>
+              <td className="py-2 px-4">{category.id} </td>
+              <td className="py-2 px-4">{category.name}</td>
+              <td className="py-2 px-4">{category.description}</td>
+              <td className="py-2 px-4 hidden md:table-cell">
+                {category.slug}
+              </td>
+              <td className="py-2 px-4 flex justify-center">
+                <Link
+                  href={""}
+                  className="border-2 px-2 py-1 rounded-full bg-gray-100 hover:bg-gray-200"
+                >
+                  <FontAwesomeIcon icon={faPlus} className="text-stone-400" />
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
