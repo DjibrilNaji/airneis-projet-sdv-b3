0c446d793e98df6b95a1e2c5920e4ca89bc9972f
djibrilnaji <najidjibril@gmail.com>
2023-04-08 15:24:28 +0200

    add modifiers in contact model

--- src/api/db/models/ContactModel.js
+++ src/api/db/models/ContactModel.js
@@ -2,6 +2,12 @@ import BaseModel from "./BaseModel.js"
 
 class ContactModel extends BaseModel {
   static tableName = "contact"
+
+  static modifiers = {
+    paginate: (query, limit, page) => {
+      return query.limit(limit).offset((page - 1) * limit)
+    },
+  }
 }
 
 export default ContactModel
