8e98aaf15ee0f600d87b17b62d7b984718125527
djibrilnaji <najidjibril@gmail.com>
2023-04-09 17:00:24 +0200

    add toggle for update + add button to go back

--- src/pages/admin/users/[userId]/view.jsx
+++ src/pages/admin/users/[userId]/view.jsx
@@ -1,7 +1,16 @@
 import LayoutAdmin from "@/web/components/Admin/LayoutAdmin/LayoutAdmin"
 import config from "@/web/config"
 import routes from "@/web/routes"
+import {
+  faArrowLeft,
+  faCheck,
+  faEdit,
+  faPerson,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import axios from "axios"
+import { useRouter } from "next/router"
+import { useState } from "react"
 
 export const getServerSideProps = async ({ params, req: { url } }) => {
   const userId = params.userId
@@ -21,7 +30,62 @@ export const getServerSideProps = async ({ params, req: { url } }) => {
   }
 }
 
-const ViewUser = () => {}
+const ViewUser = (props) => {
+  const {
+    userInfo: { result },
+    userId,
+  } = props
+
+  const router = useRouter()
+  const [toggleUpdateUser, setToggleUpdateUser] = useState(true)
+
+  return (
+    <div>
+      <div className="m-4">
+        <button onClick={() => router.back()}>
+          <FontAwesomeIcon icon={faArrowLeft} /> Retour
+        </button>
+      </div>
+
+      <div className="bg-stone-100 mx-2 my-6">
+        <div className="flex items-center justify-between border-b-4 border-red-500 px-3 py-4 ">
+          <div className="flex items-center gap-4 ">
+            <FontAwesomeIcon icon={faPerson} className="h-6 text-stone-400" />
+            <h1 className="font-bold text-xl uppercase">Informations </h1>
+
+            {result.user[0].isDelete ? (
+              <span className="italic text-red-500 text-lg">
+                (Compte supprimé : id {result.user[0].id})
+              </span>
+            ) : (
+              <span className="italic text-green-500 text-lg">
+                (Compte actif : id {result.user[0].id})
+              </span>
+            )}
+          </div>
+
+          {toggleUpdateUser ? (
+            <button
+              className="flex justify-end text-stone-500 font-bold text-lg rounded"
+              onClick={() => setToggleUpdateUser(!toggleUpdateUser)}
+              title="Modifier l'utilisateur"
+            >
+              <FontAwesomeIcon icon={faEdit} className="h-7" />
+            </button>
+          ) : (
+            <button
+              className="flex justify-end text-stone-500 font-bold text-lg rounded"
+              onClick={() => setToggleUpdateUser(!toggleUpdateUser)}
+              title="Finir les modifications"
+            >
+              <FontAwesomeIcon icon={faCheck} className="h-7" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 ViewUser.getLayout = function (page) {
   return <LayoutAdmin>{page}</LayoutAdmin>
