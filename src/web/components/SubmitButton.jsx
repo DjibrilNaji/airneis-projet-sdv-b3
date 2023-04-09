794adbbfd30bab74cbbefed1a65fa9725d092145
djibrilnaji <najidjibril@gmail.com>
2023-04-09 17:06:25 +0200

    update button and submit button

--- src/web/components/SubmitButton.jsx
+++ src/web/components/SubmitButton.jsx
@@ -1,11 +1,24 @@
 import Button from "@/web/components/Button.jsx"
+import classNames from "classnames"
 import { useFormikContext } from "formik"
 
 const SubmitButton = (props) => {
-  const { children = "Submit", isSubmitting, isValid } = useFormikContext()
+  const {
+    children = "Submit",
+    isSubmitting,
+    isValid,
+    variant,
+    size,
+    color,
+    className,
+  } = useFormikContext()
 
   return (
-    <Button {...props} disabled={isSubmitting || !isValid}>
+    <Button
+      {...props}
+      disabled={isSubmitting || !isValid}
+      className={classNames(variant, size, color, className)}
+    >
       {children}
     </Button>
   )
