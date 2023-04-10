e5fc5446003827546f13f484a8f89a3e629df899
djibrilnaji <najidjibril@gmail.com>
2023-04-10 18:43:18 +0200

    add endpoint to get products by slug

---
+++ src/pages/api/products/[slug].js
@@ -0,0 +1,53 @@
+import ImageProductModel from "@/api/db/models/ImageProductModel"
+import ProductModel from "@/api/db/models/ProductModel"
+import { NotFoundError } from "@/api/errors"
+import validate from "@/api/middlewares/validate.js"
+import mw from "@/api/mw.js"
+import { urlSlugValidator } from "@/validators"
+import s3 from "@@/configAWS"
+
+const handler = mw({
+  GET: [
+    validate({
+      query: {
+        slug: urlSlugValidator.required(),
+      },
+    }),
+    async ({
+      locals: {
+        query: { slug },
+      },
+      res,
+    }) => {
+      const product = await ProductModel.query().where({ slug })
+
+      if (!product) {
+        throw new NotFoundError()
+      }
+
+      const imageProduct = await ImageProductModel.query().where({
+        productId: product[0].id,
+      })
+
+      if (!imageProduct) {
+        throw new NotFoundError()
+      }
+
+      imageProduct.map((image) => {
+        image.urlImage = s3.getSignedUrl("getObject", {
+          Bucket: "airness-matd",
+          Key: image.urlImage,
+        })
+      })
+
+      res.send({
+        result: {
+          product,
+          imageProduct,
+        },
+      })
+    },
+  ],
+})
+
+export default handler
