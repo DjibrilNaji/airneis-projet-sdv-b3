19f75486c75e64be893c759f3a05dc463b70dc9c
djibrilnaji <najidjibril@gmail.com>
2023-04-11 23:47:49 +0200

    I retrieve products in same category and image of these products

--- src/pages/api/products/[slug].js
+++ src/pages/api/products/[slug].js
@@ -1,3 +1,4 @@
+import CategoryModel from "@/api/db/models/CategoryModel"
 import ImageProductModel from "@/api/db/models/ImageProductModel"
 import ProductModel from "@/api/db/models/ProductModel"
 import { NotFoundError } from "@/api/errors"
@@ -19,7 +20,9 @@ const handler = mw({
       },
       res,
     }) => {
-      const product = await ProductModel.query().where({ slug })
+      const product = await ProductModel.query()
+        .where({ slug })
+        .where({ isDelete: false })
 
       if (!product) {
         throw new NotFoundError()
@@ -40,10 +43,77 @@ const handler = mw({
         })
       })
 
+      const productCategory = await CategoryModel.query().where({
+        id: product[0].categoryId,
+      })
+
+      const productsInCategory = await ProductModel.query()
+        .where({
+          categoryId: productCategory[0].id,
+        })
+        .whereNot({ slug: slug })
+
+      if (!productsInCategory) {
+        throw new NotFoundError()
+      }
+
+      const imageProductsInCategory = []
+
+      for (const product of productsInCategory) {
+        const images = await ImageProductModel.query().where({
+          productId: product.id,
+        })
+
+        if (!images) {
+          throw new NotFoundError()
+        }
+
+        for (const image of images) {
+          imageProductsInCategory.push(image)
+        }
+      }
+
+      imageProductsInCategory.map((image) => {
+        image.urlImage = s3.getSignedUrl("getObject", {
+          Bucket: "airness-matd",
+          Key: image.urlImage,
+        })
+      })
+
+      const randomProducts = []
+      const randomMainImage = []
+
+      const maxProducts = Math.min(6, productsInCategory.length)
+
+      while (randomProducts.length < maxProducts) {
+        const randomIndex = Math.floor(
+          Math.random() * productsInCategory.length
+        )
+        const randomProduct = productsInCategory[randomIndex]
+
+        if (!randomProducts.includes(randomProduct)) {
+          randomProducts.push(randomProduct)
+
+          const image = imageProductsInCategory.find(
+            (image) => image.productId === randomProduct.id && image.isMain
+          )
+
+          if (image) {
+            randomMainImage.push({
+              productId: randomProduct.id,
+              urlImage: image.urlImage,
+            })
+          }
+        }
+      }
+
       res.send({
         result: {
           product,
           imageProduct,
+          productCategory,
+          randomProducts,
+          randomMainImage,
         },
       })
     },
