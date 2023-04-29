d30db9e6a0eddf0d0f65b99325a480a78c33dd5a
djibrilnaji <najidjibril@gmail.com>
2023-04-29 19:11:45 +0200

    add cart Context with function to add product in cart

---
+++ src/web/hooks/cartContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react"
+
+export const CartContext = createContext()
+
+export const CartContextProvider = (props) => {
+  const [cart, setCart] = useState(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const items = localStorage.getItem("cart")
+
+      if (items) {
+        return JSON.parse(items)
+      }
+    }
+
+    return []
+  })
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart))
+  }, [cart])
+
+  const addToCart = (product, image, quantity) => {
+    const existingProduct = cart.find((item) => item.id === product.id)
+
+    if (existingProduct) {
+      if (existingProduct.quantity === product.stock) {
+        return
+      } else {
+        setCart((prevItems) =>
+          prevItems.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + quantity }
+              : item
+          )
+        )
+      }
+    } else {
+      setCart((prevItems) => [
+        ...prevItems,
+        { ...product, img: image, quantity: quantity },
+      ])
+    }
+  }
+
+  return (
+    <CartContext.Provider
+      {...props}
+      value={{
+        actions: { addToCart },
+        state: { cart },
+      }}
+    />
+  )
+}
+
+const useCartContext = () => useContext(CartContext)
+
+export default useCartContext
