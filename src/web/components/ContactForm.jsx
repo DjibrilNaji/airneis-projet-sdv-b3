39061b25cbeddbbc603a39c72cf992fef1b48bb2
djibrilnaji <najidjibril@gmail.com>
2023-05-16 13:59:45 +0200

    add translate for contact page + for contact Form

--- src/web/components/ContactForm.jsx
+++ src/web/components/ContactForm.jsx
@@ -5,24 +5,33 @@ import React from "react"
 import { faEnvelope, faPerson } from "@fortawesome/free-solid-svg-icons"
 import FormError from "./FormError"
 import SubmitButton from "./SubmitButton.jsx"
-
-const defaultInitialValues = {
-  email: "",
-  subject: "",
-  message: "",
-}
-
-const defaultValidationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email("Email invalide")
-    .required("L'email est obligatoire")
-    .label("Email"),
-  subject: yup.string().label("Subject"),
-  message: yup.string().required("Le message est obligatoire").label("Message"),
-})
+import { useTranslation } from "next-i18next"
+import { useRouter } from "next/router"
 
 const ContactForm = (props) => {
+  const { t } = useTranslation("contact")
+  const { locale } = useRouter()
+  const direction = t("direction", { locale })
+
+  const defaultInitialValues = {
+    email: "",
+    subject: "",
+    message: "",
+  }
+
+  const defaultValidationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email(t("yup_email"))
+      .required(t("yup_email_required"))
+      .label(t("yup_email_label")),
+    subject: yup.string().label(t("yup_subject_label")),
+    message: yup
+      .string()
+      .required(t("yup_email_required"))
+      .label(t("yup_email_label")),
+  })
+
   const {
     onSubmit,
     initialValues = defaultInitialValues,
@@ -41,24 +50,27 @@ const ContactForm = (props) => {
 
         <Form className="flex flex-col gap-4 p-4">
           <FormField
+            dir={direction}
             name="email"
             type="email"
-            label="E-mail*"
+            label={`${t("email")} *`}
             icon={faEnvelope}
           />
           <FormField
+            dir={direction}
             name="subject"
             type="text"
-            label="Subject"
+            label={`${t("subject")} *`}
             icon={faEnvelope}
           />
           <FormField
+            dir={direction}
             name="message"
-            type="text"
-            label="Message*"
+            type="textarea"
+            label={`${t("message")} *`}
             icon={faPerson}
           />
-          <SubmitButton>Envoyer</SubmitButton>
+          <SubmitButton>{t("send_message")}</SubmitButton>
         </Form>
       </>
     </Formik>
