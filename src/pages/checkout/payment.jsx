7420ad3101c3ffaf4d2fd8a8ca44197acc8f4afe
djibrilnaji <najidjibril@gmail.com>
2023-06-03 14:28:30 +0200

    add checkout component and use it in payment page

---
+++ src/pages/checkout/payment.jsx
@@ -0,0 +1,101 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import cookie from "cookie"
+import { loadStripe } from "@stripe/stripe-js"
+import useCartContext from "@/web/hooks/cartContext"
+import { useEffect, useState } from "react"
+import CheckoutForm from "@/web/components/Checkout/CheckoutForm"
+import { Elements } from "@stripe/react-stripe-js"
+import config from "@/web/config"
+import routes from "@/web/routes"
+import axios from "axios"
+
+export const getServerSideProps = async ({ req, locale }) => {
+  const cookies = req.headers.cookie
+    ? cookie.parse(req.headers.cookie || "")
+    : null
+
+  const jwt = cookies ? (cookies.jwt !== undefined ? cookies.jwt : null) : null
+
+  const userId = cookies
+    ? cookies.userId !== undefined
+      ? cookies.userId
+      : null
+    : null
+
+  const addressId = cookies
+    ? cookies.addressId !== undefined
+      ? cookies.addressId
+      : null
+    : null
+
+  return {
+    props: {
+      jwt,
+      userId,
+      addressId,
+      ...(await serverSideTranslations(locale, [
+        "navigation",
+        "cart",
+        "delivery",
+        "address",
+      ])),
+    },
+  }
+}
+
+const stripePromise = loadStripe(
+  "pk_test_51MjQJFGci79RaC26CuZNM82rmkZjrFgiFYxD5Kvrczh42zTRN2xNZyVte2hj29cFzkfJiSlpJZ7onTRRPIPI121I00wYmcfLWl"
+)
+
+const Payment = () => {
+  const {
+    state: { totalStripe },
+  } = useCartContext()
+
+  const [clientSecret, setClientSecret] = useState("")
+
+  useEffect(() => {
+    const fetchClientSecret = async () => {
+      try {
+        const response = await axios.post(
+          `${config.api.baseApiURL}${routes.api.checkout.payment()}`,
+          {
+            amount: totalStripe,
+            body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+          }
+        )
+
+        setClientSecret(response.data.clientSecret)
+      } catch (error) {
+        // Handle error
+      }
+    }
+
+    const delay = setTimeout(fetchClientSecret, 100)
+
+    return () => clearTimeout(delay)
+  }, [totalStripe])
+
+  const appearance = {
+    theme: "stripe",
+  }
+
+  const options = {
+    clientSecret,
+    appearance,
+  }
+
+  return (
+    <>
+      <div className="flex">
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Payment
---
+++ src/web/components/Checkout/CheckoutForm.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react"
+import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js"
+import routes from "@/web/routes"
+import styles from "@/styles/CheckoutForm.module.css"
+
+export default function CheckoutForm() {
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const [message, setMessage] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if (!stripe) {
+      return
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    )
+
+    if (!clientSecret) {
+      return
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage(`Payment succeeded !`)
+
+          break
+
+        case "processing":
+          setMessage("Your payment is processing.")
+
+          break
+
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.")
+
+          break
+
+        default:
+          setMessage("Something went wrong.")
+
+          break
+      }
+    })
+  }, [stripe])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!stripe || !elements) {
+      return
+    }
+
+    setIsLoading(true)
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: `http://localhost:3000${routes.checkout.confirmation()}`,
+      },
+    })
+
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message)
+    } else {
+      setMessage("An unexpected error occurred.")
+    }
+
+    setIsLoading(false)
+  }
+
+  const paymentElementOptions = {
+    layout: "tabs",
+  }
+
+  return (
+    <div className="fixed inset-0">
+      <div className="flex items-center justify-center min-h-screen">
+        <form
+          className={styles.form}
+          style={{ minWidth: "300px" }}
+          onSubmit={handleSubmit}
+        >
+          <h1 className="flex justify-center font-bold text-3xl mb-6">
+            Paiement
+          </h1>
+          <PaymentElement
+            className={styles.paymentElement}
+            options={paymentElementOptions}
+          />
+          <button
+            className={styles.button}
+            disabled={isLoading || !stripe || !elements}
+            id="submit"
+          >
+            <span>
+              {isLoading ? <div className={styles.spinner}></div> : "Pay now"}
+            </span>
+          </button>
+          {message && <div className={styles.paymentMessage}>{message}</div>}
+        </form>
+      </div>
+    </div>
+  )
+}
