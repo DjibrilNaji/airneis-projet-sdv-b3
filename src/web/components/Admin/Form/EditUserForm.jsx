6cea67694feab0640a56f628cef106786e37ccb0
djibrilnaji <najidjibril@gmail.com>
2023-06-04 11:40:59 +0200

    add select for admin in bo for edit user form

--- src/web/components/Admin/Form/EditUserForm.jsx
+++ src/web/components/Admin/Form/EditUserForm.jsx
@@ -65,43 +65,38 @@ const EditUserForm = (props) => {
             label="Nom d'utilisateur :"
             active={active}
           />
-
           <FormField
             name="firstName"
             type="text"
             label="Prenom :"
             active={active}
           />
-
           <FormField
             name="lastName"
             type="text"
             label="Nom :"
             active={active}
           />
-
           <FormField
             name="email"
             type="email"
             label="E-mail* :"
             active={active}
           />
-
           <FormField
             name="password"
             type="password"
             label="Password :"
             active={active}
           />
-
-          <FormField
-            name="isAdmin"
-            type="text"
-            label="Admin ?"
-            helpText="`true` or `false`"
-            active={active}
-          />
-
+          <span>isAdmin :</span>
+          <select
+            className="font-bold border-stone-500 border focus:border-2 rounded-lg px-2 py-1 focus:outline-none disabled:border-0 disabled:cursor-text"
+            disabled={active}
+          >
+            <option value={1}>true</option>
+            <option value={2}>false</option>
+          </select>
           <SubmitButton
             active={active.toString()}
             color="light"
