f96416560078624a0b858d4889be8185c033e8bf
djibrilnaji <najidjibril@gmail.com>
2023-06-25 19:40:41 +0200

    Add components for confirmation messages for checkout and contact + add new routes and use them

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,3 +1,4 @@
+import routes from "@/web/routes"
 import Link from "next/link"
 
 function Error({ statusCode }) {
@@ -9,7 +10,7 @@ function Error({ statusCode }) {
           : "An error occurred on client"}
       </p>
       <Link
-        href={"/"}
+        href={routes.home()}
         className="my-7 bg-stone-500 text-white px-4 py-2 rounded-md text-lg"
       >
         Go Back
