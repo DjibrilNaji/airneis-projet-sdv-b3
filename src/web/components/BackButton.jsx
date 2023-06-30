0bbdd4bc60e8873f9b9ee8a4e4b5d7f1f9b51bcc
djibrilnaji <najidjibril@gmail.com>
2023-06-30 20:55:07 +0200

    Add traductions for my pages

--- src/web/components/BackButton.jsx
+++ src/web/components/BackButton.jsx
@@ -1,14 +1,16 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useTranslation } from "next-i18next"
 import { useRouter } from "next/router"
 
 const BackButton = () => {
+  const { t } = useTranslation("navigation")
   const router = useRouter()
 
   return (
     <div className="m-4">
       <button onClick={() => router.back()}>
-        <FontAwesomeIcon icon={faArrowLeft} /> Retour
+        <FontAwesomeIcon icon={faArrowLeft} /> {t("go_back")}
       </button>
     </div>
   )
